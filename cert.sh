7b147438e93fb110c4c0e3b976fd910b86b77d6f
dhiimr <51095316+dhiimr@users.noreply.github.com>
2021-11-04 20:44:25 +0700

    Update cert.sh
    Tambah spasi

--- cert.sh
+++ cert.sh
@@ -12,7 +12,7 @@ domain=$IP
 systemctl stop v2ray
 systemctl stop v2ray@none
 /root/.acme.sh/acme.sh --issue -d $domain --standalone -k ec-256
-~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt--keypath /etc/v2ray/v2ray.key --ecc
+~/.acme.sh/acme.sh --installcert -d $domain --fullchainpath /etc/v2ray/v2ray.crt --keypath /etc/v2ray/v2ray.key --ecc
 systemctl start v2ray
 systemctl start v2ray@none
 echo Done
